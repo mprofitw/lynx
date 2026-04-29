342139eeacf78d154b6fef737db1b7c094fc26f7
mprofitw <79254322+mprofitw@users.noreply.github.com>
2026-04-29 19:13:47 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const path = require('path');
-app.use(express.static(path.resolve(__dirname)));
+app.use(express.static(path.resolve(__dirname, '../')));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../index.htm'));
+});
+//app.use(express.static(path.resolve(__dirname)));
 //app.use(express.static) // Zakładając, że index.htm jest w folderze 'public'
 const express = require("express")
 const mongoose = require("mongoose")
