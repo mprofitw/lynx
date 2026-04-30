0508c79c8c65e2da1cdc7abdd1c4b73f7bd73277
mprofitw <79254322+mprofitw@users.noreply.github.com>
2026-04-30 21:36:26 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express()
 // 2. MIDDLEWARE
 app.use(cors())
 app.use(express.json())
-
+const folderGłówny = path.resolve(__dirname, '..');
 // 3. POŁĄCZENIE Z BAZĄ
 mongoose
 	.connect(process.env.MONGO_URI)
